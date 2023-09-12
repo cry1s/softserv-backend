6168a073c4055fb9636eb698661c7ce131f7a60e
cry1s <cry1s@ya.ru>
2023-09-12 17:56:27 +0000

    cool page 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use actix_web::{web, get, Responder, HttpResponse};
-use serde::Serialize;
 use handlebars::Handlebars;
+use serde::Serialize;
+
+use crate::model::Software;
+
+mod model;
 
 pub fn init_handlebars() -> Handlebars<'static> {
     let mut handlebars = Handlebars::new();
@@ -8,27 +12,18 @@ pub fn init_handlebars() -> Handlebars<'static> {
     handlebars
 }
 
-// Структура для представления данных о софте
-#[derive(Serialize)]
-struct Software {
-    name: String,
-    version: String,
-}
-
 #[get("/")]
 async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
-    let software_list = vec![
-        Software {
-            name: "Программа 1".to_string(),
-            version: "1.0".to_string(),
-        },
-        Software {
-            name: "Программа 2".to_string(),
-            version: "2.0".to_string(),
-        },
-    ];
+    let software_list = model::get_software_list();
 
-    let body = hb.render("index", &software_list);
+    #[derive(Serialize)]
+    struct RenderData {
+        software_list: Vec<Software>
+    }
+
+    let body = hb.render("index", &RenderData {
+        software_list
+    });
     
     match body {
         Ok(body) => HttpResponse::Ok().body(body),
---
+++ src/model.rs
@@ -0,0 +1,109 @@
+use serde::Serialize;
+
+// Структура для представления данных о софте
+#[derive(Serialize)]
+pub struct Software {
+    id: i32,
+    name: String,
+    version: String,
+    description: String,
+    logo: String,
+    os: String,
+    source: String,
+    tags: Vec<Tag>,
+}
+
+#[derive(Serialize)]
+pub struct Tag {
+    name: String
+}
+
+pub fn get_software_list() -> Vec<Software> {
+    vec![
+        Software {
+            id: 0,
+            name: String::from("Software 1"),
+            version: String::from("1.0"),
+            description: String::from("This is software 1"),
+            logo: String::from("logo1.png"),
+            os: String::from("Windows"),
+            source: String::from("github.com/software1"),
+            tags: vec![
+                Tag {
+                    name: String::from("tag1"),
+                },
+                Tag {
+                    name: String::from("tag2"),
+                },
+            ],
+        },
+        Software {
+            id: 1,
+            name: String::from("Software 2"),
+            version: String::from("2.0"),
+            description: String::from("This is software 2"),
+            logo: String::from("logo2.png"),
+            os: String::from("Linux"),
+            source: String::from("github.com/software2"),
+            tags: vec![
+                Tag {
+                    name: String::from("tag3"),
+                },
+                Tag {
+                    name: String::from("tag4"),
+                },
+            ],
+        },
+        Software {
+            id: 2,
+            name: String::from("Software 3"),
+            version: String::from("3.0"),
+            description: String::from("This is software 3"),
+            logo: String::from("logo3.png"),
+            os: String::from("MacOS"),
+            source: String::from("github.com/software3"),
+            tags: vec![
+                Tag {
+                    name: String::from("tag5"),
+                },
+                Tag {
+                    name: String::from("tag6"),
+                },
+            ],
+        },
+        Software {
+            id: 3,
+            name: String::from("Software 4"),
+            version: String::from("4.0"),
+            description: String::from("This is software 4"),
+            logo: String::from("logo4.png"),
+            os: String::from("Windows"),
+            source: String::from("github.com/software4"),
+            tags: vec![
+                Tag {
+                    name: String::from("tag7"),
+                },
+                Tag {
+                    name: String::from("tag8"),
+                },
+            ],
+        },
+        Software {
+            id: 4,
+            name: String::from("Software 5"),
+            version: String::from("5.0"),
+            description: String::from("This is software 5"),
+            logo: String::from("logo5.png"),
+            os: String::from("Linux"),
+            source: String::from("github.com/software5"),
+            tags: vec![
+                Tag {
+                    name: String::from("tag9"),
+                },
+                Tag {
+                    name: String::from("tag10"),
+                },
+            ],
+        }
+    ]
+}
